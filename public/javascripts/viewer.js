4382da3f45afd64ebe74d8100dced9ab34566fb5
novasmedley <novasmedley@ucla.edu>
2015-05-07 23:58:32 -0700

    get data table to show, still buggy

--- public/javascripts/viewer.js
+++ public/javascripts/viewer.js
@@ -48,59 +48,14 @@ $(document).ready(function () {
 
 function getRawDataOptions()
 {
-	$("#buttonsDiv").append('<input type="button" onclick="showRawData()" value="Raw Data" id="rawDataButton" />');
+	$("#rawData").empty();
 
 	$("#rawData").append('<div id="rawTable"></div>');
 	$("#rawData").append('<div style="float: right"> <input type="button" value="Done" id="rawDoneButton" /> </div>');
-
 	$('#rawData').jqxWindow({
 		width: 600, height: 400, resizable: true,
 		okButton: $("#rawDoneButton"), autoOpen: false
 	});
-}
-
-function showRawData()
-{
-	var name = networkInfoArray[2][0].modelname;
-	var columns = networkInfoArray[3][0].columnnames;
-	console.log(columns);
-	$('#rawData').jqxWindow('title', name);
-
-	var url = 'public/raw-data/' + name + ".csv";
-	var data = [];
-	var columnStruct = [];
-	for (var i=0; i<columns.length; i++) {
-		data[i] = {};
-		data[i]["name"] = columns[i];
-		data[i]["type"] = "float";
-
-		columnStruct[i] = {};
-		columnStruct[i]["text"] = columns[i];
-		columnStruct[i]["dataField"] = columns[i];
-		columnStruct[i]["cellsFormat"] = 'f';
-	}
-
-	var source =
-	{
-		dataType: "csv",
-		dataFields: data,
-		url: url
-	};
-
-	var dataAdapter = new $.jqx.dataAdapter(source);
-	$("#rawTable").jqxDataTable(
-		{
-			width: 850,
-			source: dataAdapter,
-			columnsResize: true,
-			columns: columnStruct
-		});
-
-
-	$("#rawData").jqxWindow('open');
-
-
-	$("#rawData").jqxWindow('open');
 }
 
 function nodeSelected(nodeID, nodeName)
@@ -160,4 +115,45 @@ function truncateOutcome(name)
 		}
 	}
 	return truncated;
+}
+
+function showRawData()
+{
+	var name = networkInfoArray[2][0].modelname;
+	var columns = networkInfoArray[3][0].columnnames;
+	console.log(columns);
+	$('#rawData').jqxWindow("setTitle", name);
+
+	var url = "assets/raw-data/" + name + ".csv";
+	var data = [];
+	var columnStruct = [];
+	for (var i=0; i<columns.length; i++) {
+		data[i] = {};
+		data[i]["name"] = columns[i];
+		data[i]["type"] = "float";
+
+		columnStruct[i] = {};
+		columnStruct[i]["text"] = columns[i];
+		columnStruct[i]["dataField"] = columns[i];
+		columnStruct[i]["cellsFormat"] = 'f';
+	}
+
+	var source =
+	{
+		dataType: "csv",
+		dataFields: data,
+		url: url
+	};
+
+	var dataAdapter = new $.jqx.dataAdapter(source);
+	$("#rawTable").jqxDataTable(
+		{
+			width: "100%",
+			height: "100%",
+			source: dataAdapter,
+			columnsResize: true,
+			columns: columnStruct
+		});
+
+	$("#rawData").jqxWindow('open');
 }
